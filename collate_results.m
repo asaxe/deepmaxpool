e52757ecbd4103563a25b4d97953bc1d1f646ef3
Andrew Michael Saxe <asaxe@stanford.edu>
2014-11-24 08:28:08 -0800

    new mnist grid

--- collate_results.m
+++ collate_results.m
@@ -3,7 +3,7 @@ function res = collate_results(expt_nm)
 fs = dir(sprintf('~/deepmaxpool/results/expt%d/*.mat',expt_nm));
 
 for i = 1:length(fs)
-    load(fs.name)
+	  load(sprintf('~/deepmaxpool/results/expt%d/%s',expt_nm,fs(i).name))
     res(i,:) = [params_tr.wNr params_tr.Nfilt params_tr.mNr best_ts ...
                 best_lambda];
     
