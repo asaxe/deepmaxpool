3988b6898049205bfac95a496d3e9db37827399c
Andrew Saxe <asaxe@stanford.edu>
2014-12-02 00:17:01 -0800

    collate stores Npcs

--- collate_results.m
+++ collate_results.m
@@ -4,7 +4,7 @@ fs = dir(sprintf('~/deepmaxpool/results/expt%d/*.mat',expt_nm));
 
 for i = 1:length(fs)
 	  load(sprintf('~/deepmaxpool/results/expt%d/%s',expt_nm,fs(i).name))
-    res(i,:) = [params_tr.wNr params_tr.Nfilt params_tr.mNr best_ts ...
+    res(i,:) = [params_tr.wNr params_tr.Nfilt params_tr.mNr params_tr.Npc best_ts ...
                 best_lambda params_tr.N];
     
 
